38d1b3b73e59bffdd2eeeed77f651e723afcfa55
gunxiaoshi <gunxiaoshi@gmail.com>
2019-06-25 14:46:56 +0800

    This is my study of Flask.

---
+++ app/tt.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: gxs
+@license: (C) Copyright 2016-2019, Light2Cloud (Beijing) Web Service Co., LTD
+@contact: dingjianfeng@light2cloud.com
+@software: L2CloudCMP
+@file: tt.py
+@ide: PyCharm
+@time: 2019/6/25 14:46
+@desc:
+"""
